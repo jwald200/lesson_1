ca4093b35d7cb7c1dcb0124fae82908b47be825f
Jacob <schoen575@gmail.com>
2015-10-26 17:37:49 -0400

    code improvements

--- rps.rb
+++ rps.rb
@@ -1,10 +1,9 @@
-choices = { 'r' => 'Rock', 'p' => 'Paper', 's' => 'Scissors' }
-WINNING_SCENARIOS = { 'r' => ['r', 's'], 'p' => ['r', 'p'], 's' => ['s', 'p'] }
+CHOICES = { 'r' => 'Rock', 'p' => 'Paper', 's' => 'Scissors' }
+WINNING_SCENARIOS = { 'r' => ['r', 's'], 'p' => ['p', 'r'], 's' => ['p', 's'] }
 player = { 'wins' => 0 }
 computer = { 'wins' => 0 }
 
 def winner?(game)
-  WINNING_SCENARIOS.each { |_choice, scenario| scenario.sort! }
   WINNING_SCENARIOS.values.include?(game.sort!)
 end
 
@@ -19,30 +18,29 @@ def winner_name(player, computer, game)
 end
 
 puts 'welcome! to Rock, Paper, Scissors game'
-puts 'How many times whould you want to play?'
+puts 'How many times whould you like to play?'
 rounds = gets.chomp.to_i
 games_played = 0
 
-while rounds > 0
+rounds.times do
   begin
     puts 'Please choose [r] for Rock, [p] for Paper, or [s] for Scissors.'
     input = gets.chomp.downcase
-  end until choices.keys.include?(input)
+  end until CHOICES.keys.include?(input)
   
   player['pick'] = input
-  computer['pick'] = choices.keys.sample
+  computer['pick'] = CHOICES.keys.sample
   game = [player['pick'], computer['pick']]
 
-  puts "Player's hand: #{choices[player['pick']]}
-   | Computer's hand: #{choices[computer['pick']]}"
+  puts "Player's hand: #{CHOICES[player['pick']]}"
+  puts "Computer's hand: #{CHOICES[computer['pick']]}"
   if winner?(game)
     puts "#{winner_name(player, computer, game)} won!"
   else
     puts "It's a tie"
   end
   puts "Player's wins: #{player['wins']} computer's wins: #{computer['wins']}"
-
-  rounds -= 1
+  
   games_played += 1
 end
 
