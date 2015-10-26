24a865d1f98a9242791401701e48d9572bf49d7e
Jacob <schoen575@gmail.com>
2015-10-26 14:10:01 -0400

    styling corrections

--- rps.rb
+++ rps.rb
@@ -1,14 +1,14 @@
-choices = {'r' => 'Rock', 'p' => 'Paper', 's' => 'Scissors' }
-WINNING_SCENARIOS = {'r' => ['r','s'], 'p' => ['r', 'p'], 's' => ['s', 'p']}
-player = {'wins' => 0}
-computer = {'wins' => 0}
+choices = { 'r' => 'Rock', 'p' => 'Paper', 's' => 'Scissors' }
+WINNING_SCENARIOS = { 'r' => ['r', 's'], 'p' => ['r', 'p'], 's' => ['s', 'p'] }
+player = { 'wins' => 0 }
+computer = { 'wins' => 0 }
 
 def winner?(game)
-  WINNING_SCENARIOS.each {|choice,scenario| scenario.sort! }
+  WINNING_SCENARIOS.each { |_choice, scenario| scenario.sort! }
   WINNING_SCENARIOS.values.include?(game.sort!)
 end
 
-def winner_name(player, computer ,game)
+def winner_name(player, computer, game)
   if player['pick'] == WINNING_SCENARIOS.key(game)
     player['wins'] += 1
     'Player'
@@ -18,15 +18,14 @@ def winner_name(player, computer ,game)
   end
 end
 
-
 puts 'welcome! to Rock, Paper, Scissors game'
-puts "How many times whould you want to play?"
+puts 'How many times whould you want to play?'
 rounds = gets.chomp.to_i
 games_played = 0
 
 while rounds > 0
   begin
-    puts "Please choose [r] for Rock, [p] for Paper, or [s] for Scissors."  
+    puts 'Please choose [r] for Rock, [p] for Paper, or [s] for Scissors.'
     input = gets.chomp.downcase
   end until choices.keys.include?(input)
   
@@ -34,7 +33,8 @@ while rounds > 0
   computer['pick'] = choices.keys.sample
   game = [player['pick'], computer['pick']]
 
-  puts "Player's hand: #{choices[player['pick']]} | Computer's hand: #{choices[computer['pick']]}"
+  puts "Player's hand: #{choices[player['pick']]}
+   | Computer's hand: #{choices[computer['pick']]}"
   if winner?(game)
     puts "#{winner_name(player, computer, game)} won!"
   else
@@ -42,8 +42,8 @@ while rounds > 0
   end
   puts "Player's wins: #{player['wins']} computer's wins: #{computer['wins']}"
 
-rounds -= 1
-games_played += 1
+  rounds -= 1
+  games_played += 1
 end
 
 puts "Nice job! You've played #{games_played} games."
